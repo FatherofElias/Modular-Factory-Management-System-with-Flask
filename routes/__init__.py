51bfa37d4ae2fc66a2dcf4e3926007a90aa370a3
FatherofElias <supra_3_8@hotmail.com>
2024-11-26 21:11:55 -0600

    update

--- routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .customer_routes import bp as customer_bp
-from .employee_routes import bp as employee_bp
-from .order_routes import bp as order_bp
-from .product_routes import bp as product_bp
-from .production_routes import bp as production_bp
