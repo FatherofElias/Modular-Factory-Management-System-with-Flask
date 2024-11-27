d707796379bd254d998de0db9b7d027e32722465
FatherofElias <supra_3_8@hotmail.com>
2024-11-26 19:46:50 -0600

    update

--- app.py
+++ app.py
@@ -15,10 +15,22 @@ def create_app(config_name):
     db.init_app(app)
     ma.init_app(app)
 
+    from routes.customer_routes import bp as customer_bp
+    from routes.employee_routes import bp as employee_bp
+    from routes.order_routes import bp as order_bp
+    from routes.product_routes import bp as product_bp
+    from routes.production_routes import bp as production_bp
+
+    app.register_blueprint(customer_bp)
+    app.register_blueprint(employee_bp)
+    app.register_blueprint(order_bp)
+    app.register_blueprint(product_bp)
+    app.register_blueprint(production_bp)
+
     return app
 
 if __name__ == '__main__':
-    app =  create_app('DevelopmentConfig')
+    app = create_app('DevelopmentConfig')
 
     with app.app_context():
         db.drop_all()
