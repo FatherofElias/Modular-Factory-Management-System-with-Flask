1001b2f432bd75d7c71a065d38476c475b1e791b
FatherofElias <supra_3_8@hotmail.com>
2024-12-03 14:57:50 -0600

    update

--- tests/test_customer_endpoints.py
+++ tests/test_customer_endpoints.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from app import create_app, db
+from app import create_app
 
 class TestCustomerEndpoints(unittest.TestCase):
     def setUp(self):
